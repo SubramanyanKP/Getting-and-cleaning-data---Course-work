a26a00bcdb67ef80bdd846f391e236c84a6349b4
SubramayanKP <46884443+SubramayanKP@users.noreply.github.com>
2019-02-10 17:01:03 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,13 @@ my_analysis <- function(){
   # Each row of subject_test.txt/subject_train.txt identifies the subject who performed the activity for each window sample.
   # Its range is from 1 to 30.
   
+  # load lookup information from the features and activity labels.txt
+ 
+  my_activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
+  my_activities$activityLabel <- gsub("_", "", as.character(my_activities$activityLabel))
+  my_features <- read.table("UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
+  included_Features <- grep("-mean\\(\\)|-std\\(\\)", my_features$featureLabel)
+  
   # load test data and read the contents to three varables from the test directory of UCI HAR Dataset  
   my_subject_testdata = read.table("UCI HAR Dataset/test/subject_test.txt")
   X_testdata = read.table("UCI HAR Dataset/test/X_test.txt")
@@ -14,13 +21,7 @@ my_analysis <- function(){
   my_subject_traindata = read.table("UCI HAR Dataset/train/subject_train.txt")
   X_traindata = read.table("UCI HAR Dataset/train/X_train.txt")
   Y_traindata = read.table("UCI HAR Dataset/train/Y_train.txt")
-  
-  # load lookup information from the features and activity_labels.txt
-  my_features <- read.table("UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
-  my_activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
-  my_activities$activityLabel <- gsub("_", "", as.character(my_activities$activityLabel))
-  included_Features <- grep("-mean\\(\\)|-std\\(\\)", my_features$featureLabel)
-  
+   
   # merge test and training data and name them
   my_subject <- rbind(my_subject_testdata, my_subject_traindata)
   names(my_subject) <- "subjectId"
